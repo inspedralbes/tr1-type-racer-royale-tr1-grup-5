6a100ab706ad532ff4bc00b6be3ae9d855a92458
Varitoo9 <alvarosaldanaaa@gmail.com>
2025-10-28 16:12:27 +0100

    agregem l'opció de transferirAdmin , expulsarJugadors, si l'usuari esta preparat i quan l'admin pot començar el joc

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,9 @@ const io = new Server(server, { cors: { origin: "*" } });
 
 app.get("/", (req, res) => res.send("Backend Type Racer Royale listo 🏁"));
 
-const jugadors = [];
+/*només guarda l'ID com a clau i despres posem un objecte com valor aquesta seria l'estructura 
+"abc123" : {id: "abc123" , name:"Tony", preparat: false, admin:false  }*/
+const jugadors = {};
 
 function broadcastPlayerList() {
   io.emit("updatePlayerList", Object.values(jugadors));
@@ -17,24 +19,7 @@ function broadcastPlayerList() {
 
 //Començen amb la connexió del servidor
 io.on("connection", (socket) => {
-    socket.on("setPlayer", (player) => {
-        if (jugadors.length == 0 ) {
-            player.admin = true;
-        }
-        jugadors.push(player)
-    }
-    
-    const admin = Object.values(jugadors).some(j => j.admin);
-
-    jugadors {
-        id: socket.id,
-        name: name,
-        preparat: false, 
-        admin: !admin 
-    }
-    
   console.log("Jugador conectat:", socket.id);
-
 
   // Desconnexió de l'usuari
   socket.on("disconnect", () => {
@@ -45,23 +30,64 @@ io.on("connection", (socket) => {
 
   // Quan un usuari ens envia el seu nom
   socket.on("setPlayerName", (name) => {
-    jugadors[socket.id] = { id: socket.id, name: name };
-    console.log(`L'usuari ${socket.id} ara es diu: ${name}`);
+    if (!name) return;
+
+    const admin = Object.values(jugadors).some((j) => j.admin);
+
+    jugadors[socket.id] = {
+      id: socket.id,
+      name: name,
+      preparat: false,
+      admin: !admin, // el primero en unirse será admin
+    };
+    console.log(`L'usuari ${name}  s'ha unit`);
     broadcastPlayerList(); // Enviem la llista actualitzada a tothom
   });
 
-  //escoltem l'ordre de quan l'usuari li dona a preparat 
+  //escoltem l'ordre de quan l'usuari li dona a preparat
   socket.on("usuariPreparat", () => {
     jugadors[socket.id].preparat = !jugadors[socket.id].preparat;
 
-    console.log(`Jugador ${jugadors[socket.id].name} preparat: ${jugadors[socket.id].preparat}`);
-    .name
+    console.log(
+      `Jugador ${jugadors[socket.id].name} preparat: ${
+        jugadors[socket.id].preparat
+      }`
+    );
     broadcastPlayerList();
   });
 
-  socket.on("usuariAdmin", () =>  {
+  //Escolta quan expulsem al jugador que te el idJugador
+  socket.on("expulsarJugador", (idJugador) => {
+    if (!jugadors[socket.id] || !jugadors[socket.id].admin) return;
 
-  })
+    if (jugadors[idJugador]) {
+      io.to(idJugador).emit("expulsat");
+      delete jugadors[idJugador];
+      console.log(`Jugador ${idJugador} ha estat expulsat per l'admin`);
+      broadcastPlayerList();
+    }
+  });
+
+  socket.on("transferirAdmin", (idNuevoAdmin) => {
+    if (!jugadors[socket.id] || !jugadors[socket.id].admin) return;
+
+    if (jugadors[idNuevoAdmin]) {
+      jugadors[socket.id] = false;
+      jugadors[idNuevoAdmin] = true;
+
+      broadcastPlayerList();
+    }
+  });
+
+  socket.on("IniciarJoc", () => {
+    if (!jugadors[socket.id] || !jugadors[socket.id].admin) return;
+
+    io.emit("JocIniciat");
+  });
+
+  socket.on("JocIniciat", () => {
+    //En aquest apartat canviarem la vista
+  });
 });
 
 const PORT = process.env.PORT || 3000;
