e5454d4b10f111387f40817bf93b93959c5a6659
Varitoo9 <alvarosaldanaaa@gmail.com>
2025-11-04 19:42:03 +0100

    importo ref ja que no s'importaba

--- frontend/src/components/PreGame/lobby/viewLobby.vue
+++ frontend/src/components/PreGame/lobby/viewLobby.vue
@@ -1,34 +1,44 @@
 <template>
-    <p>Benvingut {{ jugadorClient.name }} en aquesta partida tens el rol de {{ jugadorClient.role }}</p>
-<!-- Llista pel admin-->
-	<div>
-		<playerList :socket-c="socket" :llista-jug="llistaJugadors" :is-admin="isAdmin" :jugador="jugadorClient"/>
-		<!--Botons-->
-		<button v-if="isAdmin" v-bind:class="isMajority ? '' : 'disabled'" @click="startGame">
-			Començar
-		</button>
-		<button v-bind:class="imReady ? 'ready' : 'notReady'" @click="toggleReady(jugadorClient.id)">
-			Preparat
-		</button>
-	</div>
+  <p>
+    Benvingut {{ jugadorClient.name }} en aquesta partida tens el rol de {{ jugadorClient.role }}
+  </p>
+  <!-- Llista pel admin-->
+  <div>
+    <playerList
+      :socket-c="socket"
+      :llista-jug="llistaJugadors"
+      :is-admin="isAdmin"
+      :jugador="jugadorClient"
+    />
+    <!--Botons-->
+    <button v-if="isAdmin" v-bind:class="isMajority ? '' : 'disabled'" @click="startGame">
+      Començar
+    </button>
+    <button v-bind:class="imReady ? 'ready' : 'notReady'" @click="toggleReady(jugadorClient.id)">
+      Preparat
+    </button>
+  </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
-import playerList from './playerList.vue';
+import { ref, computed } from 'vue'
+import playerList from './playerList.vue'
 
 //props
 const props = defineProps(['socketC', 'llistaJug', 'jug'])
-const socket = computed(() => props.socketC);
-const llistaJugadors = computed(() => props.llistaJug);
-const jugadorClient = computed(() => props.jug || {});
+const socket = computed(() => props.socketC)
+const llistaJugadors = computed(() => props.llistaJug)
+const jugadorClient = computed(() => props.jug || {})
 
 const imReady = ref(false)
 
 // Computed per actualitzar-se quan canviïn les dades
 const isMajority = computed(() => {
   if (!llistaJugadors || !Array.isArray(llistaJugadors)) return false
-  return llistaJugadors.filter(player => player.isReady === true).length >= Math.round(llistaJugadors.length/2)
+  return (
+    llistaJugadors.filter((player) => player.isReady === true).length >=
+    Math.round(llistaJugadors.length / 2)
+  )
 })
 
 const isAdmin = computed(() => {
@@ -37,20 +47,18 @@ const isAdmin = computed(() => {
 
 //sockets
 
-
 //funcions
-function startGame(){
+function startGame() {
   if (socket && jugadorClient?.id) {
     socket.emit('startGame', { id: jugadorClient.id })
   }
 }
 
-function toggleReady(id){
+function toggleReady(id) {
   if (socket && id) {
     socket.emit('setIsReady', { id: id })
   }
 }
-
 </script>
 
 <style scoped></style>
