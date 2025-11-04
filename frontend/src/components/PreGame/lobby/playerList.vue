8884580c1cdfe332f34c8a2a787313d66c8550ca
Tonn-nnay <tymartinmarin@gmail.com>
2025-11-04 10:08:11 +0100

    Estandarització comunicació amb servidor

--- frontend/src/components/PreGame/lobby/playerList.vue
+++ frontend/src/components/PreGame/lobby/playerList.vue
@@ -8,17 +8,18 @@
             <button v-if="player.id > 0" @click="deletePlayer(player.id)">
                 <!--Logo creu-->
             </button>
-            <div class="estat" v-bind:class="player.preparat === true ? 'ready' : 'notReady'"></div>
+            <div class="estat" v-bind:class="player.isReady === true ? 'ready' : 'notReady'"></div>
         </li>
     </ul>
 		<!-- Llista per jugador / espectador -->
     <ul v-else>
         <li v-for="player in props.llistaJug" :key="player.id">
             {{ player.name }}
-            <div class="estat" v-bind:class="player.preparat === true ? 'ready' : 'notReady'"></div>
+            <div class="estat" v-bind:class="player.isReady === true ? 'ready' : 'notReady'"></div>
         </li>
     </ul>
 </template>
+
 <script setup>
 const props = defineProps(['socketC', 'llistaJug', 'isAdmin', 'jugador'])
 
@@ -28,13 +29,13 @@ const socket = props.socketC;
 //Functions
 function setAdmin(id){
   if (socket && props.isAdmin && props.jugador?.id) {
-    socket.emit('transferirAdmin', { adminId: props.jugador.id, idNuevoAdmin: id });
+    socket.emit('transferAdmin', { adminId: props.jugador.id, newAdminId: id });
   }
 }
 
 function deletePlayer(id){
   if (socket && props.isAdmin && props.jugador?.id) {
-    socket.emit('expulsarJugador', { adminId: props.jugador.id, idJugador: id });
+    socket.emit('kickPlayer', { adminId: props.jugador.id, playerId: id });
   }
 }
 </script>
